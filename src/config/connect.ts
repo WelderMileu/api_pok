117aa5e6978904a165d0dafbda01a036888ca880
welder mileu <welderfelix21@gmail.com>
2022-04-06 17:00:58 -0300

    alterando o banco de dados sqlite3 para postgresql

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -1,6 +1,16 @@
 import { Sequelize } from "sequelize"
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-export const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./src/database/database.sqlite"
+const database = process.env.DATABASE as string
+const user = process.env.USER as string
+const pass = process.env.PASS
+
+export const sequelize = new Sequelize(
+  database,
+  user,
+  pass,
+{
+  host: "localhost",
+  dialect: 'postgres'
 })
