e427035a1764ff3842e377c4be29e37eab97e132
welder mileu <welderfelix21@gmail.com>
2022-04-06 21:03:49 -0300

    alterando a conexao com o banco de dados para formato url

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -3,11 +3,5 @@ import { config } from 'dotenv'
 
 config()
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASS,
-{
-  host: process.env.DB_HOST,
-  dialect: 'postgres'
-})
+const URL = process.env.DATABASE_URL as string
+export const sequelize = new Sequelize(URL)
