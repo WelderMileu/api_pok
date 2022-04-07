4dc478472a50ddd10f9c7c5b4ae4d635550bbda1
welder mileu <welderfelix21@gmail.com>
2022-04-06 21:38:46 -0300

    adicionando ssl na coneção com o postgres

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -4,4 +4,4 @@ import { config } from 'dotenv'
 config()
 
 const URL = process.env.DATABASE_URL as string
-export const sequelize = new Sequelize(URL)
+export const sequelize = new Sequelize(URL, { ssl: true })
